c38e64ca3448f689b93539125d9a7d79b3f55dcb
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-19 02:22:59 +0600

    Home HTML and Home JS Files Added

--- js/login.js
+++ js/login.js
@@ -9,9 +9,20 @@ document.getElementById("login-btn")
     .addEventListener("click", function(event) {
         // Step-2: Prevent Default Behavior (Prevent Reloading Browser)
         event.preventDefault();
-        console.log("Login button clicked");
 
         // Step-3: Get The Phone Number
         const phoneNumber = document.getElementById("phone-number").value;
-        console.log(phoneNumber);
+        const pinNumber = document.getElementById("Pin-number").value;
+        console.log(phoneNumber, pinNumber);
+
+        // Step-4: Validate Phone and Pin
+        // This is not the perfect way (temporary)
+        if (phoneNumber === "5" && pinNumber === "1234") {
+            console.log("You are logged in");
+            window.location.href = "/home.html"
+            // step-5: allow user to the website
+        }
+        else {
+            alert("Wrong Phone Number or Pin");
+        }
     })
